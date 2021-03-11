70e330e24636df231f7b2b0a2cd115f1fca11b1c
tobx <net@tobx.de>
2021-03-11 15:41:08 +0100

    Add "no sound" header notification

--- src/components/page/PageContent.svelte
+++ src/components/page/PageContent.svelte
@@ -35,8 +35,7 @@
 
   h1 {
     border-bottom: 0.125em solid var(--color-primary);
-    margin-bottom: 0.5em;
-    margin-top: 0;
+    margin: 1em 0 0 0;
   }
 
   h2 {
@@ -44,17 +43,22 @@
   }
 
   h3 {
-    font-size: 150%;
-    font-weight: 300;
-    margin: 2em 0 1em 0;
+    margin: 2em 0 0.75em 0;
   }
 
   @media (min-width: 640px) {
     h1 {
       font-size: 400%;
+      margin: 0.75em 0 0.25em 0;
     }
     h2 {
       font-size: 250%;
+      margin: 2em 0 0.75em 0;
+    }
+    h3 {
+      font-size: 150%;
+      font-weight: 300;
+      margin: 2em 0 1em 0;
     }
   }
 
@@ -64,19 +68,20 @@
     flex-direction: column;
     align-items: center;
     padding: 1em;
+    padding-bottom: 3em;
   }
 
   @media (min-width: 640px) {
     main {
-      padding: 1em 2em 4em 2em;
+      padding-bottom: 6em;
     }
   }
 
   /* https://webkit.org/blog/7929/designing-websites-for-iphone-x/ */
   @supports (padding: max(0px)) {
     main {
-      padding-left: max(2em, env(safe-area-inset-left));
-      padding-right: max(2em, env(safe-area-inset-right));
+      padding-left: max(1em, env(safe-area-inset-left));
+      padding-right: max(1em, env(safe-area-inset-right));
     }
   }
 
